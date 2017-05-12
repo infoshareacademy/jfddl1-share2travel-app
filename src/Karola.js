02ab89be5a51560aafc0a6e424034cd8ba855247
karolina <karolina.joanna.skiba@gmail.com>
2017-05-12 16:58:10 +0200

    form

--- src/Karola.js
+++ src/Karola.js
@@ -1,39 +1,70 @@
 /**
  * Created by karolina on 10.05.17.
  */
-import React from 'react';
+import React, {Component} from 'react';
 import products from './data/products'
 import './App.css';
 import './Karola.css';
+import Form from './Form'
+
 // import {
 //     BrowserRouter as Router,
 //     Route
 // } from 'react-router-dom'
+// let prodyctArray = products;
+
+class Karola extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            products,
+            prodyctArray: products,
+        };
+
+    }
 
 
+    onDataChange(value) {
+        let prodyctArray = this.state.prodyctArray.filter((product) => {
+            if (product.product.includes(value)) {
+                return product
+            }
+        })
 
-        const Karola = () => (
+        if(prodyctArray.length === 0){
+            prodyctArray = this.state.products;
+        }
+        console.log('prodyctArray', prodyctArray);
 
+        this.setState({
+            prodyctArray
+        })
+    }
+
+    render() {
+        return (
             <div>
                 <h1>Products</h1>
+                <Form onUserChange={this.onDataChange.bind(this)}/>
+
                 <ul>
                     {
-                        products.map(
-                            dat => (
-                                <li key={dat.id}>
+                        this.state.prodyctArray.map(
+                            (dat, index) => (
+                                <li key={index}>
                                     {dat.product},{dat.price},{dat.color}, {dat.producer}
                                 </li>
                             )
                         )
-
-                        // students.map(function (student) {
-                        //   return <li key={student.id}>{student.name}</li>
-                        // })
                     }
                 </ul>
             </div>
 
 
         )
+    }
+
+}
 
 export default Karola
