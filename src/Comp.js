f092522600d749714d4c32cb83ae745a05319e56
Home <home@Homes-MacBook-Pro.local>
2017-05-17 16:38:51 +0200

    one more button css fix

--- src/Comp.js
+++ src/Comp.js
@@ -1,66 +1,72 @@
 import React from 'react';
-import { Grid, Row, Col, Tab, Tabs, ButtonToolbar, Button, Panel} from 'react-bootstrap';
+import { Grid, Row, Col, ButtonToolbar, Button, Panel} from 'react-bootstrap';
 import Chart from './Chart'
 import './Comp.css'
 
 class Comp extends React.Component{
-		render(){
-				return (
-					<Grid>
-						<Panel className='center' bsStyle="primary" header='Nazwa produktu'>
-   				 	</Panel>
-						<Row className='firstRow vertical-align'>
-							<Col className='firstRowImage' lg={4}>
-								<img src='shoe.jpg' alt=''/>
-							</Col>
-							<Col className='opis' lg={4}>
-								<p>
-								Typ produktu: "Keyboard"<br/>
-								Kolor: ivory<br/>
-								Materiał: Soft<br/>
-								Producent: Schinner Group<br/>
-								</p>
-							</Col>
-							<Col className='firstRowButtons' lg={4}>
-									<ButtonToolbar>
-									<Button bsStyle="info" id='but1' >
-										<img className='left' src='nike.png' alt=''/>
-											<span className='price'>
-												1800zł
-											</span>
-										</Button>
-									<Button bsStyle="info" id='but2' >
-										<img src='nike.png' alt=''/>
-											<span className='price'>
-												1200zł
-											</span>
-										</Button>
-									<Button bsStyle="info" id='but3' >
-										<img src='nike.png' alt=''/>
-											<span className='price'>
-												1100zł
-											</span>
-										</Button>
-									<Button bsStyle="info" id='but4' >
-										<img src='nike.png' alt=''/>
-											<span className='price'>
-												1000zł
-											</span>
-										</Button>
-									</ButtonToolbar>
-							</Col>
-						</Row>
-						<Row>	
-							<Col className='wykresik' lg={6}>
-								<Chart series={[{data: [209.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6]}]}/>
-							</Col>
-							{/*<Col className='trend' xs={12} sm={12} md={6} lg={6}>
-							
-							</Col>*/}
-						</Row>
-					</Grid>
-				)
-		}
+    render(){
+        return (
+            <Grid>
+                <Panel className='center' bsStyle="primary" header='Nazwa produktu'>
+                </Panel>
+
+                <Row className='vertical-align'>
+                    <Col lg={4}>
+                        <img src='shoe.jpg' alt=''/>
+                    </Col>
+                    <Col className='opis' lg={4}>
+                        <h1>Opis produktu</h1>
+                        <p>
+                        Typ produktu: "Keyboard"<br/>
+                        Kolor: ivory<br/>
+                        Materiał: Soft<br/>
+                        Producent: Schinner Group<br/>
+                        </p>
+                    </Col>
+                    <Col className='firstRowButtons' lg={4}>
+                        <ButtonToolbar>
+                            <Button bsStyle="info" id='but1' >
+                                <img className='left' src='nike.png' alt=''/>
+                                <span className='price'>
+                                    1800zł
+                                </span>
+                            </Button>
+                            <Button bsStyle="info" id='but2' >
+                                <img src='nike.png' alt=''/>
+                                <span className='price'>
+                                    1200zł
+                                </span>
+                            </Button>
+                            <Button bsStyle="info" id='but3' >
+                                <img src='nike.png' alt=''/>
+                                <span className='price'>
+                                    1100zł
+                                </span>
+                            </Button>
+                            <Button bsStyle="info" id='but4' >
+                                <img src='nike.png' alt=''/>
+                                <span className='price'>
+                                    1000zł
+                                </span>
+                            </Button>
+                            <Button bsStyle="info" id='but5' >
+                                <img className='left' src='nike.png' alt=''/>
+                                <span className='price'>
+                                    1800zł
+                                </span>
+                            </Button>
+                        </ButtonToolbar>
+                    </Col>
+                </Row>
+
+                <Row>
+                    <Col className='wykresik' lg={6}>
+                        <Chart series={[{data: [209.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6]}]}/>
+                    </Col>
+                </Row>
+            </Grid>
+        )
+    }
 }
  
 export default Comp 
