ea04db49869bb0bb4e01da6f834ad75821bfe75e
Piotr <piotr.golianek@outlook.com>
2017-05-30 17:03:44 +0200

    piotr-login-init

--- src/index.js
+++ src/index.js
@@ -11,17 +11,18 @@ import store from './store'
 import App from './App';
 import './index.css';
 
-// import * as firebase from 'firebase'
-//
-// var config = {
-//   apiKey: "AIzaSyCseH8VVh3E2xhD-vvvDvK1xi4QD6hSOrE",
-//   authDomain: "projektf-b3d8e.firebaseapp.com",
-//   databaseURL: "https://projektf-b3d8e.firebaseio.com",
-//   projectId: "projektf-b3d8e",
-//   storageBucket: "projektf-b3d8e.appspot.com",
-//   messagingSenderId: "454308380303"
-// };
-// firebase.initializeApp(config);
+import * as firebase from 'firebase';
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAG6Ev8evQ6BAbT6X-Xo0iTygMLFAMXWZk",
+  authDomain: "to-buy-login.firebaseapp.com",
+  databaseURL: "https://to-buy-login.firebaseio.com",
+  projectId: "to-buy-login",
+  storageBucket: "to-buy-login.appspot.com",
+  messagingSenderId: "999600320545"
+};
+firebase.initializeApp(config);
 
 ReactDOM.render(
     <Provider store={store}>
