dc3135e5ea7ce5331babd5f373087bc4d44e61aa
karolina <karolina.joanna.skiba@gmail.com>
2017-06-01 16:57:32 +0200

    favorites in firebase is workinh here

--- src/index.js
+++ src/index.js
@@ -15,15 +15,14 @@ import * as firebase from 'firebase';
 
 // Initialize Firebase
 var config = {
-  apiKey: "AIzaSyAG6Ev8evQ6BAbT6X-Xo0iTygMLFAMXWZk",
-  authDomain: "to-buy-login.firebaseapp.com",
-  databaseURL: "https://to-buy-login.firebaseio.com",
-  projectId: "to-buy-login",
-  storageBucket: "to-buy-login.appspot.com",
-  messagingSenderId: "999600320545"
+  apiKey: "AIzaSyCF4_K4fufQdHxs0cr3wYekXFs37Vu2qkE",
+  authDomain: "share2travel-1994c.firebaseapp.com",
+  databaseURL: "https://share2travel-1994c.firebaseio.com",
+  projectId: "share2travel-1994c",
+  storageBucket: "share2travel-1994c.appspot.com",
+  messagingSenderId: "385410885455"
 };
 firebase.initializeApp(config);
-
 ReactDOM.render(
     <Provider store={store}>
       <App />
--- src/state/favoriteProducts.js
+++ src/state/favoriteProducts.js
@@ -1,32 +1,41 @@
 /**
  * Created by Karolina on 21.05.2017.
  */
-export const ADD='favoriteProducts/ADD'
-export const REMOVE='favoriteProducts/REMOVE'
+export const ADD = 'favoriteProducts/ADD'
+export const REMOVE = 'favoriteProducts/REMOVE'
+import * as firebase from 'firebase'
 
 export const add = (productId) => ({
-    type: ADD,
-    productId: productId
+  type: ADD,
+  productId: productId
 })
 
 export const remove = (productId) => ({
-    type: REMOVE,
-    productId: productId
+  type: REMOVE,
+  productId: productId
 })
+
 const initialState = []
 export default (state = initialState, action) => {
-    switch(action.type) {
-        case ADD:
-            if (state.includes(action.productId)) {
-                return state
-            } else {
-                return [...state, action.productId]
-            }
-        case REMOVE:
-            return state.filter(
-                productId => productId !== action.productId
-            )
-        default:
-            return state
-    }
+  switch (action.type) {
+    case ADD:
+      var newState;
+      if (state.includes(action.productId)) {
+        newState = state;
+      } else {
+        newState = [...state, action.productId]
+      }
+      var userId = firebase.auth().currentUser.uid;
+      firebase.database().ref('/').child('favourites').child(userId).set(newState)
+        .then(() => {
+         console.log('IT WORKS!');
+        });
+      return newState;
+    case REMOVE:
+      return state.filter(
+        productId => productId !== action.productId
+      )
+    default:
+      return state
+  }
 }
